19089443c1bf1d2ecd718aef7b44afa399c6e26e
yair-roshal <yair7705778@gmail.com>
2023-07-24 20:04:44 +0300

    fix

--- server.js
+++ server.js
@@ -28,12 +28,55 @@ app.use('/', routes)
 
 // app.listen(port, host, () => console.log(`Server listens http://${host}:${port}`))
 
+//temp =============================
 
+const mysql = require('mysql2')
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
 
+const upload = multer({ dest: 'uploads/' })
+const constants = require('./constants/constants')
 
+const pool = mysql.createPool(constants.sqlConfig)
+
+const executeSqlFile = (filePath) => {
+  const sql = fs.readFileSync(filePath, 'utf-8')
+  return new Promise((resolve, reject) => {
+    pool.query(sql, (err, results) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(results)
+      }
+    })
+  })
+}
+
+app.post('/api/uploadSql', upload.single('sqlFile'), (req, res) => {
+  const { file } = req
+
+  if (!file) {
+    res.status(400).json({ error: 'No SQL file provided' })
+    return
+  }
+
+  const filePath = path.join(__dirname, file.path)
+
+  executeSqlFile(filePath)
+    .then(() => {
+      res.sendStatus(200)
+      console.log('1qqqqqq',)
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Error updating table', details: error.message })
+    })
+    .finally(() => {
+      fs.unlinkSync(filePath) // Удаляем временный файл после выполнения
+    })
+})
 
 //===============================
-
 
 // Create an HTTPS server and listen on port 443
 https.createServer(httpsOptions, app).listen(443, () => {
